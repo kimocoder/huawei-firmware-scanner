03332952a0f2986f0791de3cc249fc4ac447a29a
Pekaikon Norckon <dfc643@acgdraw.com>
2016-06-23 15:29:12 +0800

    Linux Version: updated

--- linux/HuaWei-Firmware-Scanner.sh
+++ linux/HuaWei-Firmware-Scanner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# @Author:
-# @Date:
+# @Author: 56304289@qq.com
+# @Date: 2016.06.21
 # @Last Modified by:   anchen
-# @Last Modified time:
+# @Last Modified time: 2016.06.19
 
 echo "请输入手机型号: TL00 TL00H UL00"
 read model
@@ -23,7 +23,7 @@ Filter(){
     if [[ $1 == "1" ]]; then
         File="$PWD/$model/$i.xml"
         Size=`ls -il $File | awk '{print $6}'`
-        if [ $Size == "162" ] || [ $Size == "0" ];then
+        if [ $Size == "162" ] || [ $Size == "107" ] || [ $Size == "0" ];then
             echo
             echo "This is Not to Download File, Will be Deleted!"
             echo "File:$File"
@@ -47,24 +47,43 @@ Filter(){
 
 Filter
 
+GetCount=0
+declare -i GetCount
+MaxCount=100
+declare -i MaxCount
+
 echo "您打算从哪个版本开始查询（推荐从 40172 [B535] 开始）："
 read query_version
+echo "请输入结束查询的版本号:"
+read e
 
-for ((i=$query_version;i<99999;i++));do
-    echo
-    echo "正在查询版本 v$i，若想终止直接关闭窗口"
-    echo
-    changelog_url="http://update.hicloud.com:8180/TDS/data/files/p3/s15/G1022/g$ml/v$i/f1/full/changelog.xml"
-    curl $changelog_url > $PWD/$model/$i.xml
+for ((i=$query_version;i<$e;i++));do
+    if [ $GetCount != $MaxCount ]; then
+        echo
+        echo "正在查询版本 v$i，若想终止直接关闭窗口"
+        echo
+        changelog_url="http://update.hicloud.com:8180/TDS/data/files/p3/s15/G1022/g$ml/v$i/f1/full/changelog.xml"
+        curl $changelog_url > $PWD/$model/$i.xml
 
-    Filter 1
+        Filter 1
+        GetCount=GetCount+1
+    else
+        echo "Stop 30s"
+        sleep 30
+        GetCount=0
+    fi
 done
 
-for xml in `find $PWD/$model`;do
-    vars=(`basename -s .xml $xml`)
-    echo "可下载版本:"
-    echo "${vars[$@]}"
-done
+echo "可下载版本:"
+#for xml in `find $PWD/$model`;do
+#    vs=(`basename -s .xml $xml`)
+#    vars=(${vs[*]})
+#    echo "${vars[*]}"
+#done
+
+vs=(`find $PWD/$model -name *.xml`)
+vars=(`basename -s .xml ${vs[*]}`)
+echo "${vars[*]}"
 
 echo "请输入要下载的版本:"
 read var
@@ -75,12 +94,24 @@ update_url="http://update.hicloud.com:8180/TDS/data/files/p3/s15/G$ml/g223/v$var
 
 
 Download(){
-    if [ $1 == "axel" ]||[ `which axel` ]; then
-        axel -n 16 $update_url
-    elif [ $1 == "curl" ]||[ `which curl` ]; then
-        curl $update_url
-    elif [ $1 == "wget" ]||[ `which wget` ]; then
-        wget $update_url
+    if [ $1 == "axel" ];then
+        if [ `which axel` ]; then
+            axel -n 16 $update_url
+        else
+            echo "Not Install $1!"
+        fi
+    elif [ $1 == "curl" ];then
+        if [ `which curl` ]; then
+            curl $update_url
+        else
+            echo "Not Install $1!"
+        fi
+    elif [ $1 == "wget" ];then
+        if [ `which wget` ]; then
+            wget $update_url
+        else
+            echo "Not Install $1!"
+        fi
     else
         echo "Not Install $1!"
     fi
@@ -99,4 +130,6 @@ case $d in
         Download $d
     ;;
     *)
+        echo "Not Install $1!"
     ;;
+esac
--- linux/TL00H.sh
+++ linux/TL00H.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# @Author:
-# @Date:
+# @Author: 56304289@qq.com
+# @Date: 2016.06.21
 # @Last Modified by:   anchen
-# @Last Modified time:
+# @Last Modified time: 2016.06.19
 
 echo "您打算从哪个版本开始查询（推荐从 40172 [B535] 开始）："
 read v
+echo "请输入结束查询的版本号:"
+read e
 
 Filter(){
     if [[ $1 == "1" ]]; then
         File="$PWD/TL00H/$i.xml"
         Size=`ls -il $File | awk '{print $6}'`
-        if [ $Size == "162" ] || [ $Size == "0" ];then
+        if [ $Size == "162" ] || [ $Size == "107" ] || [ $Size == "0" ];then
             echo
             echo "This is Not to Download File, Will be Deleted!"
             echo "File:$File"
@@ -20,8 +22,8 @@ Filter(){
         fi
     else
         for f in `ls $PWD/TL00H`; do
-            File=`ls -il $PWD/TL00H/$i.xml | awk '{print $10}'`
-            Size=`ls -il $PWD/TL00H/$i.xml | awk '{print $6}'`
+            File=`ls -il $PWD/TL00H/$f | awk '{print $10}'`
+            Size=`ls -il $PWD/TL00H/$f | awk '{print $6}'`
             if [ $Size == "162" ] || [ $Size == "0" ];then
                 echo
                 echo "This is Not to Download File, Will be Deleted!"
@@ -35,21 +37,38 @@ Filter(){
 
 Filter
 
-for ((i=$v;i<99999;i++));do
-    echo
-    echo "正在查询版本 v$i，若想终止直接关闭窗口"
-    echo
-    changelog_url="http://update.hicloud.com:8180/TDS/data/files/p3/s15/G1022/g223/v$i/f1/full/changelog.xml"
-    curl $changelog_url > $PWD/TL00H/$i.xml
+GetCount=0
+declare -i GetCount
+MaxCount=100
+declare -i MaxCount
 
-    Filter 1
+for ((i=$v;i<$e;i++));do
+    if [ $GetCount != $MaxCount ]; then
+        echo
+        echo "正在查询版本 v$i，若想终止直接关闭窗口"
+        echo
+        changelog_url="http://update.hicloud.com:8180/TDS/data/files/p3/s15/G1022/g223/v$i/f1/full/changelog.xml"
+        curl $changelog_url > $PWD/TL00H/$i.xml
+
+        Filter 1
+        GetCount=GetCount+1
+    else
+        echo "Stop 30s"
+        sleep 30
+        GetCount=0
+    fi
 done
 
-for xml in `find $PWD/TL00H`;do
-    vars=(`basename -s .xml $xml`)
-    echo "可下载版本:"
-    echo "${vars[$@]}"
-done
+echo "可下载版本:"
+#for xml in `find $PWD/TL00H`;do
+#    vs=(`basename -s .xml $xml`)
+#    vars=(${vs[*]})
+#    echo "${vars[*]}"
+#done
+
+vs=(`find $PWD/TL00H -name *.xml`)
+vars=(`basename -s .xml ${vs[*]}`)
+echo "${vars[*]}"
 
 echo "请输入要下载的版本:"
 read var
@@ -59,12 +78,24 @@ read d
 update_url="http://update.hicloud.com:8180/TDS/data/files/p3/s15/G1022/g223/v$var/f1/full/update.zip"
 
 Download(){
-    if [ $1 == "axel" ]||[ `which axel` ]; then
-        axel -n 16 $update_url
-    elif [ $1 == "curl" ]||[ `which curl` ]; then
-        curl $update_url
-    elif [ $1 == "wget" ]||[ `which wget` ]; then
-        wget $update_url
+    if [ $1 == "axel" ];then
+        if [ `which axel` ]; then
+            axel -n 16 $update_url
+        else
+            echo "Not Install $1!"
+        fi
+    elif [ $1 == "curl" ];then
+        if [ `which curl` ]; then
+            curl $update_url
+        else
+            echo "Not Install $1!"
+        fi
+    elif [ $1 == "wget" ];then
+        if [ `which wget` ]; then
+            wget $update_url
+        else
+            echo "Not Install $1!"
+        fi
     else
         echo "Not Install $1!"
     fi
@@ -83,4 +114,6 @@ case $d in
         Download $d
     ;;
     *)
+        echo "Not Install $1!"
     ;;
+esac
